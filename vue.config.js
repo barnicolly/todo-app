647d659763a5193d9f0d6c2c66e8391753949268
MishaRatnikov <mik009@yandex.ru>
2024-03-05 21:04:02 +0300

    Исправление warning Feature flag __VUE_PROD_HYDRATION_MISMATCH_DETAILS__ is not explicitly defined

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   publicPath: './',
   lintOnSave: 'warning',
+  chainWebpack: (config) => {
+    config.plugin('define').tap((definitions) => {
+      Object.assign(definitions[0], {
+        __VUE_OPTIONS_API__: 'true',
+        __VUE_PROD_DEVTOOLS__: process.env.NODE_ENV === 'development',
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: process.env.NODE_ENV === 'development',
+      });
+      return definitions;
+    });
+  },
 };
